97452cd593d3d4afb3489f4c95bdac6d3d0b9194
tofuhoney <skd0529@gmail.com>
2025-03-26 23:23:10 +0900

    assignment6

---
+++ pages/01_documentgpt.py
@@ -0,0 +1,136 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
+import streamlit as st
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
+
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use the following portion of a long document to see if any of the text is relevant to answer the question. Return any relevant text verbatim. If there is no relevant text, return : ''
+            -------
+            {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
+"""
+)
+
+with st.sidebar:
+    api_key = st.text_input("OpenAI API Key")
+
+
+if api_key:
+    llm = ChatOpenAI(
+        temperature=0.1,
+        streaming=True,
+        callbacks=[
+            ChatCallbackHandler(),
+        ],
+        api_key=api_key,
+    )
+else:
+    llm = ChatOpenAI(
+        temperature=0.1,
+        streaming=True,
+        callbacks=[
+            ChatCallbackHandler(),
+        ],
+    )
+
+splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    separator="\n",
+    chunk_size=600,
+    chunk_overlap=100,
+)
+
+cache_dir = LocalFileStore("./.cache/")
+loader = UnstructuredFileLoader("files/1984.txt")
+docs = loader.load_and_split(text_splitter=splitter)
+embeddings = OpenAIEmbeddings()
+cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+vectorstore = FAISS.from_documents(docs, cached_embeddings)
+retriever = vectorstore.as_retriever()
+send_message("I'm ready! Ask away!", "ai", save=False)
+paint_history()
+message = st.chat_input("Ask anything about your file...")
+if message:
+    send_message(message, "human")
+    chain = (
+        {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | llm
+    )
+    with st.chat_message("ai"):
+        chain.invoke(message)
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
